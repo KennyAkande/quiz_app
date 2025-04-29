8aaa146027e2cd3d6ee1a57cd78e1761f45c29ea
Akande Kehinde <kehindeakande067@gmail.com>
2025-04-29 17:10:20 +0100

    add javascript code to make my questions interactive

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
     $(".clicky").click(function () {
-        $(".main").fadeOut(300);
-        $(".display").fadeIn(300);
+        $(".main").fadeOut(300, function () {
+            $(".display").fadeIn(300);
+        });
     });
+    let timeleft = 10;
+    let timer;
+
+    timer = setInterval(function(){
+        timeleft--;
+        $("#time").text(timeleft);
+
+        if (timeleft <= 0) {
+            clearInterval (timer);
+            
+        }
+    }, 1000)
+
 });
