972a2de569507203e73d647f620b4f5f42df3012
Akande Kehinde <kehindeakande067@gmail.com>
2025-04-29 17:37:39 +0100

    add debugging javascript code

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
     $(".clicky").click(function () {
         $(".main").fadeOut(300, function () {
-            $(".display").fadeIn(300);
+            $(".display1").fadeIn(300);
         });
     });
-    let timeleft = 10;
+    let timeleft = 11;
     let timer;
+    let currentQuestion = 1;
 
     timer = setInterval(function(){
         timeleft--;
-        $("#time").text(timeleft);
+        $(".time").text(timeleft);
 
         if (timeleft <= 0) {
             clearInterval (timer);
-            
+            $(".display" + currentQuestion).hide();
+            currentQuestion++;
+            $(".display" + currentQuestion).show();
         }
     }, 1000)
 
